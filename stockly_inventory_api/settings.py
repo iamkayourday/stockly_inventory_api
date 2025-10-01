8f0c05b43695070aa223bc3f0fa498168af1dbe6
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-01 14:17:36 +0100

    Refactor settings.py to organize installed apps and update database configuration for PostgreSQL

--- stockly_inventory_api/settings.py
+++ stockly_inventory_api/settings.py
@@ -31,17 +31,27 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # LOCAL APPS
+    'inventory',
+
+    # THIRD PARTY APPS
+     'rest_framework',
+     'rest_framework_simplejwt',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -73,13 +83,29 @@ WSGI_APPLICATION = 'stockly_inventory_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stockly_inventory',
+        'USER': 'stockly_admin',
+        'PASSWORD': 'stockly2025.',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+# Rest Framework and JWT configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
 
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
