eb194e32b38c1f08fbbcd591e0ef1347e53377b1
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-01 20:20:47 +0100

    Implement user registration view with appropriate permissions and serializer

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser,IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
+from .serializers import UserRegistrationSerializer
+from .models import CustomUser
 
 # Create your views here.
+# This view handles user registration
+class UserRegistrationView(CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+
+
