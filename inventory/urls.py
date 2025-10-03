1990a5b64028083ef93f997ba4507f5d9aabfafe
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-03 23:09:44 +0100

    Add category-related URL patterns for listing, creating, retrieving, updating, and deleting categories

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import UserRegistrationView, UserListView, PasswordChangeView
+from .views import UserRegistrationView, UserListView, PasswordChangeView,  CategoryListView,CategoryCreateView,CategoryDetailView,CategoryUpdateView,CategoryDeleteView
+
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
@@ -9,4 +10,9 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('change-password/', PasswordChangeView.as_view(), name='change_password'),
     path('users/', UserListView.as_view(), name='user_list'),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/create/', CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
 ]
