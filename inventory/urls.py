0cbe27173f5f654d3b451813616d99a6dc4c0f28
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-03 21:27:42 +0100

    Add user registration, login, and password change endpoints to URL configuration

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserListView, PasswordChangeView
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user_registration'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('change-password/', PasswordChangeView.as_view(), name='change_password'),
+    path('users/', UserListView.as_view(), name='user_list'),
 ]
