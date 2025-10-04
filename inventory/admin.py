bd285cd119427eaef3655b300714cce79e587f72
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-04 01:02:12 +0100

    Add InventoryItem model and update migrations for custom user and category IDs

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,22 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Profile, Category
+from .models import CustomUser, Profile, Category, InventoryItem
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
-    list_display = [
-        'id',
-        'email',
-        'username',
-        'first_name',
-        'middle_name',
-        'last_name',
-        'phone_number',
-        'is_staff',
-        'is_active',
-        'last_login',
-        'date_joined',
+    list_display = ['id', 'email', 'username', 'first_name', 'middle_name', 'last_name', 'phone_number', 'is_staff', 'is_active', 'last_login', 'date_joined',
     ]
 
     fieldsets = (
@@ -43,6 +32,14 @@ class ProfileAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
     list_display = ['id', 'name', 'created_at', 'updated_at']
+
+class InventoryItemAdmin(admin.ModelAdmin):
+    model = InventoryItem
+    list_display = ['id', 'name', 'description', 'quantity', 'price', 'category', 'low_stock_threshold', 'created_at', 'updated_at']
+    search_fields = ['name', 'category__name']
+    list_filter = ['category', 'created_at', 'updated_at']
+
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(InventoryItem, InventoryItemAdmin)
