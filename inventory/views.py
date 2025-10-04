73f43ef1461cc1756dfb639053e0bd8cafeeeee5
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-04 01:44:47 +0100

    feat:

--- inventory/views.py
+++ inventory/views.py
@@ -4,8 +4,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser,IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
-from .serializers import UserRegistrationSerializer, UserListSerializer, PasswordChangeSerializer, CategorySerializer
-from .models import CustomUser, Category
+from .serializers import UserRegistrationSerializer, UserListSerializer, PasswordChangeSerializer, CategorySerializer, InventoryItemSerializer
+from .models import CustomUser, Category, InventoryItem
 
 # Create your views here.
 # This view handles user registration
@@ -51,6 +51,8 @@ class PasswordChangeView(UpdateAPIView):
 
 
 # Category views will go here to Add, List, Update, Retrieve, Delete Categories
+
+# List Categories
 class CategoryListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -58,22 +60,60 @@ class CategoryListView(ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'created_at', 'updated_at']
 
+# Create Category
 class CategoryCreateView(CreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAdminUser]
 
+# Retrieve Category
 class CategoryDetailView(RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+# Update Category
 class CategoryUpdateView(UpdateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAdminUser]
 
+# Delete Category
 class CategoryDeleteView(DestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAdminUser]
+
+# Inventory Item views will go here to Add, List, Update, Retrieve, Delete Inventory Items
+
+# List inventory Items
+class InventoryItemListView(ListAPIView):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'category', 'price', 'quantity', 'created_at', 'updated_at']
+    search_fields = ['name', 'description', 'category__name']
+    ordering_fields = ['name', 'price', 'quantity', 'created_at', 'updated_at']
+    ordering = ['-updated_at']
+
+class InventoryCreateView(CreateAPIView):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class UserInventoryListView(ListAPIView):
+    serializer_class = InventoryItemSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'category', 'price', 'quantity', 'created_at', 'updated_at']
+    search_fields = ['name', 'description', 'category__name']
+
+    def get_queryset(self):
+        return InventoryItem.objects.filter(user=self.request.user)
+
+
+    
