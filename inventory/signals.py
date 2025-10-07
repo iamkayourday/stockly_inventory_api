1dcee31d4678a27bed0f24f93681163c729778cc
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-07 11:06:32 +0100

    feat(inventory): add signal to create initial inventory change on InventoryItem creation

--- inventory/signals.py
+++ inventory/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CustomUser, Profile
+from .models import CustomUser, Profile, InventoryItem, InventoryChange
 
 
 # Signal to create or update user profile when a User instance is created or updated
@@ -10,3 +10,16 @@ def create_or_update_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     else:
         instance.profile.save()
+
+@receiver(post_save, sender=InventoryItem)
+def create_initial_inventory_change(sender, instance, created, **kwargs):
+    if created and instance.quantity > 0:
+        InventoryChange.objects.create(
+            item=instance,
+            user=instance.user,  # user can be null if created by admin
+            change_type='RESTOCK',
+            quantity_change=instance.quantity,
+            previous_quantity=0,
+            new_quantity=instance.quantity,
+            reason='Initial stock entry',
+        )
